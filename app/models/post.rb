98dce7182ae0199fd9a2584c37a99878b32936d5
nikolay <alexandroff.nikolay05@gmail.com>
2024-11-30 13:20:05 +0000

    refactor: Improve hashtag processing in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ApplicationRecord
   has_many :replies, dependent: :destroy
   has_and_belongs_to_many :tags
-  
+
   validates :body, presence: true
   validates :author, presence: true
-  
+
   after_save :process_hashtags
-  
+
   private
-  
+
   def process_hashtags
     # Find all hashtags in the body (starting with # and ending in whitespace or end of string)
     hashtags = body.scan(/#\w+(?=\s|$)/)
-    
+
     # Create or find tags and associate them with the post
     hashtags.each do |tag_name|
       tag = Tag.find_or_create_by(name: tag_name)
