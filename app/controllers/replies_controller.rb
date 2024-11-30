a576bfbb745f2f12005d706d81a11b98ff0aa698
nikolay <alexandroff.nikolay05@gmail.com>
2024-11-30 14:56:14 +0000

    fix: Remove unauthenticated access and add debug print in replies controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,18 +1,18 @@
 class RepliesController < ApplicationController
-  allow_unauthenticated_access only: [:index]
   def create
+    puts "PLEEEASE"
     @post = Post.find(params[:post_id])
     @reply = @post.replies.build(reply_params)
-    
+
     if @reply.save
-      redirect_to posts_path, notice: 'Reply was successfully created.'
+      redirect_to posts_path, notice: "Reply was successfully created."
     else
-      redirect_to posts_path, alert: 'Error creating reply.'
+      redirect_to posts_path, alert: "Error creating reply."
     end
   end
-  
+
   private
-  
+
   def reply_params
     params.require(:reply).permit(:body, :author)
   end
